bdf63cb11fd14e09536c138101a4c3be80c03a5f
Chalie <chaliewizz4@gmail.com>
2025-07-23 21:29:27 +0300

    hardcode a debiug value and add a static files folder

--- citizen/settings.py
+++ citizen/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJ_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = False
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
 else:
